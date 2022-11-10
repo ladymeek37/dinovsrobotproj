a1c8f72385c220d4a87d9f38322b108d8f8ba8dc
Lady Meek <ladyjmeek@gmail.com>
2022-11-09 16:21:54 -0800

    add files and add robot, dino, and weapon  classes according to uml

---
+++ robot.py
@@ -0,0 +1,10 @@
+from weapon import Weapon
+
+class Robot:
+    def __init__(self,name):
+        self.name = name
+        self.health = int
+        self.active_weapon = Weapon()
+    def attack(self, dinosaur):
+        pass
+
